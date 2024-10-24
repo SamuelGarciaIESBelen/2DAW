e12b2c1328dabcb9ba4aa3f0d9ca511661651a0d
Samuel <sgarzor433@g.educaand.es>
2024-10-24 21:20:50 +0200

    Form

---
+++ DWECL/JavaScript/DOM/Form/form.js
@@ -0,0 +1,5 @@
+// Agregar controlador de eventos al botón de envío
+submitButton.addEventListener('click', (event) => {
+	event.preventDefault(); // Evitar el envío del formulario
+	// Mostrar datos en un div
+})
