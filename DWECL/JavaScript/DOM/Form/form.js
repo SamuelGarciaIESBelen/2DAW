b39aaa8eace7fc2119e20c9588814bff28ce4894
Samuel <sgarzor433@g.educaand.es>
2024-10-25 10:11:59 +0200

    Form

--- DWECL/JavaScript/DOM/Form/form.js
+++ DWECL/JavaScript/DOM/Form/form.js
@@ -1,5 +1,21 @@
-// Agregar controlador de eventos al botón de envío
 submitButton.addEventListener('click', (event) => {
-	event.preventDefault(); // Evitar el envío del formulario
-	// Mostrar datos en un div
+	event.preventDefault();
+
+	const form = document.querySelector("form");
+	const labels = form.getElementsByTagName("label");
+	const inputs = form.getElementsByTagName("input");
+	let counter = 0;
+
+	let full = true;
+	for (let i = 0; i < inputs.length || full; i++) {
+		if (inputs[i].value.trim() == "") {
+			full = false;
+		}
+	}
+	if (!full) {
+		alert("All fields must be filled");
+	}
+	while (counter < inputs.length) {
+		document.querySelector(".res").innerHTML += "<p><b>" + labels[counter].textContent + "</b> " + inputs[counter++].value + "</p>";
+	}
 })
