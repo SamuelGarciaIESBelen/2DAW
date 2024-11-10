8ec94252af218db9392edd457892809ec37b7066
Samuel <sgarzor433@g.educaand.es>
2024-11-10 18:26:03 +0100

    Ejercicio libros mejorado

--- DWECL/JavaScript/JSON/books.js
+++ DWECL/JavaScript/JSON/books.js
@@ -1,111 +1,93 @@
-let libros = [
-    {
-        "titulo": "Percy Jackson and the Lightning Thief",
-        "genero": ["Fantasía", "Aventuras"],
-        "autor": ["Rick Riordan"],
-        "pags": 377,
-        "fecha": "2005-07-01",
-        "leido": true,
-        "web": ""
-    },
-    {
-        "titulo": "The Name of the Wind",
-        "genero": ["Fantasía", "Aventura"],
-        "autor": ["Patrick Rothfuss"],
-        "pags": 662,
-        "fecha": "2007-03-27",
-        "leido": false,
-        "web": "www.patrickrothfuss.com"
-    },
-    {
-        "titulo": "One Piece",
-        "genero": ["Manga", "Fantasía"],
-        "autor": ["Eiichiro Oda"],
-        "pags": 0,
-        "fecha": "1997-07-22",
-        "leido": true,
-        "web": ""
-    },
-    {
-        "titulo": "Fourth Wing",
-        "genero": ["Fantasía", "Romance"],
-        "autor": ["Rebecca Yarros"],
-        "pags": 736,
-        "fecha": "2023-08-05",
-        "leido": true,
-        "web": ""
-    },
-    {
-        "titulo": "Shadow and Bone",
-        "genero": ["Fantasía", "Romance"],
-        "autor": ["Leigh Bardugo"],
-        "pags": 358,
-        "fecha": "2012-05-03",
-        "leido": true,
-        "web": ""
-    },
-    {
-        "titulo": "Dungeons and Dragons",
-        "genero": ["Fantasía", "Rol"],
-        "autor": ["Patrick Rothfuss", "Wizards of the Coast"],
-        "pags": 320,
-        "fecha": "2014-08-19",
-        "leido": false,
-        "web": "dnd.wizards.com/"
-    }
-];
+let fillTable = (libros) => {
+	const tbody = document.querySelector("tbody");
 
-//1
-function p1(libros) {
-    let generos = [];
-    libros.map(e => e.genero).forEach(e => {
-        for (let gen of e) {
-            if (!generos.includes(gen))
-                generos.push(gen);
-        }
-    });
+	for (let libro of libros) {
+		let tr = document.createElement("tr");
+		
+		Object.values(libro).forEach(value => {
+			let td = document.createElement("td");
+			td.textContent = value;
+			tr.appendChild(td);
+		});
+		tbody.appendChild(tr);
+	}
+}
 
-    document.querySelector("#uno").innerHTML = `<p>Genres are: ${generos}</p>`
-};
-//2
+// 1. Name of each of the genres
+let p1 = (libros) => {
+	const res = document.querySelector("#uno");
+	let genres = [];
+	res.innerHTML = "";
+	
+	libros.map(l => l.genero).forEach(genero => {
+		for (g of genero) {
+			if (!genres.includes(g)) {
+				genres.push(g);
+			}
+		}
+	})
+	
+	res.innerHTML = `<p>Genres are: ${genres}</p>`;
+}
+
+// 2. Title of books with more than 300 pages
 let p2 = (libros) => {
-    document.querySelector("#dos").innerHTML = "<p>"
-    libros.filter(e => e.pags > 300).forEach(e => document.querySelector("#dos").innerHTML += `${e.titulo} (${e.pags} pages) <br><br>`);
-    document.querySelector("#dos").innerHTML += "</p>";
+	const res = document.querySelector("#dos");
+	res.innerHTML = "";
+	
+	libros.filter(l => l.pags > 300)
+		.forEach(l => res.innerHTML += `<p>${l.titulo}</p>`);
 }
 
-//3
+// 3. Title of books published more than 2 years ago
 let p3 = (libros) => {
-    libros.filter(e => e.fecha < "2022").forEach(e => document.querySelector("#tres").innerHTML += "<p>" + e.titulo + " - Date: " + e.fecha + "</p>");
+	const res = document.querySelector("#tres");
+	res.innerHTML = "";
+
+	libros.filter(l => l.fecha < "2022")
+		.forEach(l => res.innerHTML += `<p>${l.titulo} - Published in ${l.fecha}</p>`);
 }
 
-//4
+// 4. Name of the authors and number of books they have written
 let p4 = (libros) => {
-    let autorNum = {};
+	const res = document.querySelector("#cuatro");
+	let autorNum = {};
+	res.innerHTML = "";
 
-    libros.forEach(e => {
-        e.autor.forEach(a => {
-            if (autorNum[a]) {
-                autorNum[a]++;
-            } else {
-                autorNum[a] = 1;
-            }
-        });
-    });
-    for (autor in autorNum) {
-        document.querySelector('#cuatro').innerHTML += '<p>' + autor + '</p>';
-    }
+	libros.forEach(l => { l.autor.forEach(a => {
+			if (autorNum[a]) {
+				autorNum[a]++;
+			} else {
+				autorNum[a] = 1;
+			}
+		});
+	});
+	
+	for (autor in autorNum) {
+		res.innerHTML += `<p>${autor} - Books written: ${autorNum[autor]}</p>`;
+	}
 }
 
-//5
+
+// 5. Title of the books read, ordered by date of publishing
 let p5 = (libros) => {
-    libros.filter(e => e.leido)
-        .sort((a, b) => new Date(a.fecha) - new Date(b.fecha))
-        .forEach(e => document.querySelector('#cinco').innerHTML += '<p>' + e.titulo + ' - Read? ' + e.leido + ' - Date: ' + e.fecha + '</p>');
+	let res = document.querySelector("#cinco");
+	res.innerHTML = "";
+	
+	libros.sort((a, b) => new Date(a.fecha) - new Date(b.fecha))
+		.filter(l => l.leido)
+		.forEach(l => res.innerHTML += `<p>${l.titulo} - Published in ${l.fecha}</p>`);
+
 }
 
-document.querySelector('#p1').onclick = () => p1(libros);
-document.querySelector('#p2').onclick = () => p2(libros);
-document.querySelector('#p3').onclick = () => p3(libros);
-document.querySelector('#p4').onclick = () => p4(libros);
-document.querySelector('#p5').onclick = () => p5(libros);
+fillTable(libros);
+document.querySelector("#p1").onclick = () => p1(libros);
+document.querySelector("#h1").onclick = () => document.querySelector("#uno").innerHTML = "";
+document.querySelector("#p2").onclick = () => p2(libros);
+document.querySelector("#h2").onclick = () => document.querySelector("#dos").innerHTML = "";
+document.querySelector("#p3").onclick = () => p3(libros);
+document.querySelector("#h3").onclick = () => document.querySelector("#tres").innerHTML = "";
+document.querySelector("#p4").onclick = () => p4(libros);
+document.querySelector("#h4").onclick = () => document.querySelector("#cuatro").innerHTML = "";
+document.querySelector("#p5").onclick = () => p5(libros);
+document.querySelector("#h5").onclick = () => document.querySelector("#cinco").innerHTML = "";
---
+++ DWECL/JavaScript/JSON/datos.js
@@ -0,0 +1,56 @@
+const libros = [
+	{
+		"titulo": "Percy Jackson and the Lightning Thief",
+		"genero": ["Fantasía", "Aventura"],
+		"autor": ["Rick Riordan"],
+		"pags": 377,
+		"fecha": "2005-07-01",
+		"leido": true,
+		"web": ""
+	},
+	{
+		"titulo": "The Name of the Wind",
+		"genero": ["Fantasía", "Aventura"],
+		"autor": ["Patrick Rothfuss"],
+		"pags": 662,
+		"fecha": "2007-03-27",
+		"leido": false,
+		"web": "www.patrickrothfuss.com"
+	},
+	{
+		"titulo": "One Piece First Volume - Romance Dawn",
+		"genero": ["Manga", "Fantasía"],
+		"autor": ["Eiichiro Oda"],
+		"pags": "208",
+		"fecha": "1997-07-24",
+		"leido": true,
+		"web": ""
+	},
+	{
+		"titulo": "Fourth Wing",
+		"genero": ["Fantasía", "Romance"],
+		"autor": ["Rebecca Yarros"],
+		"pags": 736,
+		"fecha": "2023-08-05",
+		"leido": true,
+		"web": ""
+	},
+	{
+		"titulo": "Shadow and Bone",
+		"genero": ["Fantasía", "Romance"],
+		"autor": ["Leigh Bardugo"],
+		"pags": 358,
+		"fecha": "2012-05-03",
+		"leido": true,
+		"web": ""
+	},
+	{
+		"titulo": "Dungeons and Dragons",
+		"genero": ["Fantasía", "Rol"],
+		"autor": ["Patrick Rothfuss", "Wizards of the Coast"],
+		"pags": 120,
+		"fecha": "2014-08-19",
+		"leido": false,
+		"web": "dnd.wizards.com/"
+	}
+];
